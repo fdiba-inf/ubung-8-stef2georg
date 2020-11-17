01a6ead09001e22a268a078fc884bc226606fda0
stef2georg <stefan.georgiev91@gmail.com>
2020-11-17 08:16:20 +0200

    Ubung 8

--- run.sh
+++ run.sh
@@ -5,29 +5,14 @@ cd ~/ubung*/src
 
 echo "Directory: $(pwd)"
 
-echo -e 'Index\tJava File'
-index=0
-for file in exercise*/*.java; do 
-	echo -e "$index\t\t$file"
-	index=$((index + 1))
-done
+echo 'rm */*.class'
+rm */*.class
 
-read -p 'Enter index: ' enteredIndex
+echo 'javac exercise8/Utils.java exercise8/Point.java exercise8/PointDemo.java'
+javac exercise8/Utils.java exercise8/Point.java exercise8/PointDemo.java
 
-index=0
-for file in exercise*/*.java; do
-	if [[ "$index" == "$enteredIndex" ]]; then 
-		echo "javac *.java"
-		javac *.java
-		class=$(echo $file | sed 's/.java//' | sed 's/\//./')
-		echo "java -ea $class"
-		echo -e "\U0001f680 \U0001f648 \U0001f649 \U0001f64A \U0001f680"
-		java -ea $class
-	else 
-		:
-	fi
-
-	index=$((index + 1))
-done
+echo 'java -ea exercise8.PointDemo'
+echo -e "\U0001f680 \U0001f648 \U0001f649 \U0001f64A \U0001f680"
+java -ea exercise8.PointDemo
 
 cd $directory
